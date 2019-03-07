8776d66bfcf10543558cc11f7d41bddcfb254f4a
Frimirno <juliettegregoire34@gmail.com>
2019-03-07 17:48:25 +0100

    mouvements

--- JS/scenario.js
+++ JS/scenario.js
@@ -8,15 +8,19 @@ function mouvementClavier(event) {
     var k = event.keyCode; // event est ici un keydown, et keyCode est le code de la touche pressée
     switch (k) {
         case 37: // touche gauche
+            document.getElementById("personnage").style.transform = "rotate(0deg)";
             pers.mouvement(-5, 0);
             break;
         case 38: // touche haut
+            document.getElementById("personnage").style.transform = "rotate(90deg)";
             pers.mouvement(0, -5);
             break;
         case 39: // touche droite
+            document.getElementById("personnage").style.transform = "rotate(180deg)";
             pers.mouvement(5, 0);
             break;
         case 40: // touche bas
+            document.getElementById("personnage").style.transform = "rotate(-90deg)";
             pers.mouvement(0, 5);
             break;
     }
