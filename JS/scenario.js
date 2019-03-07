4fab1bf3d3808b7b11d10857c46481b6b2c520c5
Frimirno <juliettegregoire34@gmail.com>
2019-03-07 17:59:08 +0100

    amelioration code mvt

--- JS/scenario.js
+++ JS/scenario.js
@@ -6,21 +6,23 @@ var exo = document.getElementById("exercice");
 // functions
 function mouvementClavier(event) {
     var k = event.keyCode; // event est ici un keydown, et keyCode est le code de la touche pressée
+    let perso = document.getElementById("personnage");
+    perso.src = "IMG/sprite.gif";
     switch (k) {
         case 37: // touche gauche
-            document.getElementById("personnage").style.transform = "rotate(0deg)";
+            perso.style.transform = "rotate(0deg)";
             pers.mouvement(-5, 0);
             break;
         case 38: // touche haut
-            document.getElementById("personnage").style.transform = "rotate(90deg)";
+            perso.style.transform = "rotate(90deg)";
             pers.mouvement(0, -5);
             break;
         case 39: // touche droite
-            document.getElementById("personnage").style.transform = "rotate(180deg)";
+            perso.style.transform = "rotate(180deg)";
             pers.mouvement(5, 0);
             break;
         case 40: // touche bas
-            document.getElementById("personnage").style.transform = "rotate(-90deg)";
+            perso.style.transform = "rotate(-90deg)";
             pers.mouvement(0, 5);
             break;
     }
