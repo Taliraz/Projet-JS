aba896bbe6baf5569ee40e075babf4d144c3f6c6
Taliraz <43641531+Taliraz@users.noreply.github.com>
2019-03-21 07:29:46 +0100

    Create java.js

---
+++ JS/matieres/java.js
@@ -0,0 +1,40 @@
+class java {
+
+    construct() {}
+
+    start() {
+        mapSprite.style.display="none";
+        let p1 = document.createElement("p");
+        p1.innerHTML = "public static void main(String[]args){");"
+        exo.appendChild(p1);
+        let input = document.createElement("input");
+        input.id = "result";
+        input.style.width = "300px";
+        input.placeholder="Boucle de 0 à 9"
+        exo.append(input);
+        let p2 = document.createElement("p");
+        p2.innerHTML = "   System.out.println(i+\"/\"+n)"
+        exo.appendChild(p2);
+        let p3 = document.createElement("p");
+        p3.innerHTML = "}";
+        exo.appendChild(p3);
+        let p4 = document.createElement("p");
+        p4.innerHTML = "}"
+        exo.appendChild(p4);
+        let p5 = document.createElement("button");
+        p5.innerHTML = "Vérifier"
+        exo.appendChild(p5);
+        document.getElementsByTagName("button")[0].addEventListener("click", this.verify);
+    }
+
+    verify() {
+        var res = document.getElementById("result").value
+        res = res.replace(/\s+/g, '');
+        if (res === "for(inti=0;i<10;i++){" || res==="for(inti=0;i<=9;i++){") {
+            while (exo.firstChild) {
+                exo.removeChild(exo.firstChild);
+            }
+        }
+        mapSprite.style.display="block";
+    }
+}
