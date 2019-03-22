8bd426c68a72a836854ded7640e409ba616e1dc0
Frimirno <juliettegregoire34@gmail.com>
2019-03-22 09:46:19 +0100

    matiere.lancer fini

--- JS/matiere.js
+++ JS/matiere.js
@@ -10,20 +10,19 @@ class Matiere {
         if (this.nom == "Architecture")
             mat = new archi();
         else if (this.nom == "Bases de donnees")
-            mat = new php();
+            mat = new bd();
+        if (this.nom == "Java")
+            mat = new java();
+        if (this.nom == "JavaScript")
+            mat = new javascript();
+        if (this.nom == "Maths")
+            mat = new maths();
         if (this.nom == "PHP")
             mat = new php();
-        if (this.nom == "PHP")
-            mat = new php();
-        if (this.nom == "PHP")
-            mat = new php();
-        if (this.nom == "PHP")
-            mat = new php();
-        if (this.nom == "PHP")
-            mat = new php();
-        if (this.nom == "PHP")
-            mat = new php();
+        if (this.nom == "reseau")
+            mat = new reseau();
+        if (this.nom == "Scheme")
+            mat = new scheme();
         mat.start();
     }
-
 }
